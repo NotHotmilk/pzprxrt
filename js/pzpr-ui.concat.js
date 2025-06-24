a8d5bf05fa6644b9a313afc5575e21807d8baa59
NotHotmilk <The_Hotmilk@icloud.com>
2025-06-24 16:42:24 +0900

    Updates

--- js/pzpr-ui.concat.js
+++ js/pzpr-ui.concat.js
@@ -12,7 +12,7 @@
  * This script is released under the MIT license. Please see below.
  *  http://www.opensource.org/licenses/mit-license.php
  *
- * Date: 2025-06-23
+ * Date: 2025-06-24
  */
 // intro.js
 
